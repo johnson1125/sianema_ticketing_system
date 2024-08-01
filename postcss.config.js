cad0637d51e1f261b9c5e5512b7399f0e8664da2
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-08-01 20:09:49 +0800

    forgot to save changes for postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
 export default {
-<<<<<<< HEAD
   plugins: {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
->>>>>>> 90ccd35213fa15cdf07bf9528bb0436ff306954b
