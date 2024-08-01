8a7c13237bce702a7862325a0cf12836fd1e5a98
Johnson Ong <ong5828@gmail.com>
2024-08-01 20:34:05 +0800

    route file update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,8 +27,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-
-use App\Http\Controllers\MovieController;
 
 Route::resource('movies', MovieController::class);
