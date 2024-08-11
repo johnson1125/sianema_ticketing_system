ebc7bb42041934ef07d073190132f98bd1549f3f
Johnson Ong <ong5828@gmail.com>
2024-08-12 03:52:50 +0800

    Setup navigation bar

--- resources/views/layouts/adminNavigation.blade.php
+++ resources/views/layouts/adminNavigation.blade.php
@@ -6,19 +6,15 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
+                    <a href="{{ route('hallTimeSlot') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                        {{ __('Home') }}
-                    </x-nav-link>                   
-
                     <x-nav-link :href="route('hallTimeSlot')" :active="request()->routeIs('hallTimeSlot')">
-                        {{ __('HallTimeSlot') }}
+                        {{ __('Hall TimeSlot') }}
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Sianema') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -20,11 +20,11 @@
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <he class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
-                </header>
+                </he`der>
             @endisset
 
             <!-- Page Content -->
