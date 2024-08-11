aac9baebd7a1ee39fcfbb1f70ebe53fe8e50f611
Johnson Ong <ong5828@gmail.com>
2024-08-12 04:02:31 +0800

    update layout file

--- resources/views/admin/hallTimeSlot/index.blade.php
+++ resources/views/admin/hallTimeSlot/index.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.admin-layout>
+<x-admin-Layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Hall') }}
@@ -21,4 +21,4 @@
 
 
 
-</x-layouts.admin-layout>
+</x-admin-Layout>
