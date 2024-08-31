49e4f0d87a5b72bc9fb455ab4be4dca986063536
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-08-31 18:31:29 +0800

    initial version of footer done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sianema - @yield('title', '')</title>
-
+    
     <!-- Load CSS via Vite -->
     @vite(['resources/css/master.css', 'resources/css/app.css'])
-
+    
     <!-- Additional CSS -->
     @stack('styles')
 </head>
-
 <body class="bg-black">
     <!-- Header -->
     <header>
@@ -97,15 +95,13 @@
         </div>
     </footer>
 
-
     <!-- Common JS -->
     <script src="{{ asset('js/jquery.min.js') }}"></script>
 
     <!-- Load JS via Vite -->
     @vite(['resources/js/master.js', 'resources/js/app.js'])
-
+    
     <!-- Additional JS -->
     @stack('scripts')
 </body>
-
 </html>
