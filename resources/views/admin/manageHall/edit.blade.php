33a521f40ed2c7ea7f7622a3f1b25e75037b3013
Siays <sysheng1@hotmail.com>
2024-09-04 14:27:38 +0800

    add seat id below each seat

--- resources/views/admin/manageHall/edit.blade.php
+++ resources/views/admin/manageHall/edit.blade.php
@@ -3,75 +3,172 @@
         @csrf
         <div class="hallView">
             <div class="screen-container">
-                <img class="screenImg" src="{{ asset('images/screen.png')}}"  alt="Screen">
+                <img class="screenImg" src="{{ asset('images/screen.png') }}" alt="Screen">
             </div>
             <div class="seat-container">
                 @php
                     $rowLetters = $seats->unique('row_letter')->pluck('row_letter')->all();
                 @endphp
                 @if ($hall->hall_type === 'Standard')
-                    @for($i=0; $i<sizeof($rowLetters); $i++)
+                    @for ($i = 0; $i < sizeof($rowLetters); $i++)
                         <div class="standard-row">
                             <div class="standard-left-column">
-                                @for($j=1; $j<=3;$j++)
+                                @for ($j = 1; $j <= 3; $j++)
                                     @php
-                                        $seat = $seats->where('row_letter', $rowLetters[$i])->where('column_number', $j)->first();
+                                        $seat = $seats
+                                            ->where('row_letter', $rowLetters[$i])
+                                            ->where('column_number', $j)
+                                            ->first();
                                     @endphp
-                                    @if($seat)
-                                        @if($seat->status === 'open')
-                                        <img class="seatImg" src="{{ asset('images/singleseat.png')}}" alt="Seat {{ $seat->seat_id }}">
-                                        @else
-                                        <img class="seatImg" src="{{ asset('images/unavailableseat.png')}}" alt="Seat {{ $seat->seat_id }}">
-                                        @endif
+                                    @if ($seat)
+                                        <div class="seat-box">
+                                            @if ($seat->status === 'open')
+                                                <img class="standardSeatImg" src="{{ asset('images/singleseat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @else
+                                                <img class="standardSeatImg"
+                                                    src="{{ asset('images/unavailableseat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @endif
+                                            <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}
+                                            </div>
+                                        </div>
                                     @endif
                                 @endfor
                             </div>
                             <div class="standard-middle-column">
-                                @for($j=4; $j<=9;$j++)
+                                @for ($j = 4; $j <= 9; $j++)
                                     @php
-                                        $seat = $seats->where('row_letter', $rowLetters[$i])->where('column_number', $j)->first();
+                                        $seat = $seats
+                                            ->where('row_letter', $rowLetters[$i])
+                                            ->where('column_number', $j)
+                                            ->first();
                                     @endphp
-                                    @if($seat)
-                                        @if($seat->status === 'open')
-                                        <img class="seatImg" src="{{ asset('images/singleseat.png')}}" alt="Seat {{ $seat->seat_id }}">
-                                        @else
-                                        <img class="seatImg" src="{{ asset('images/unavailableseat.png')}}" alt="Seat {{ $seat->seat_id }}">
-                                        @endif
+                                    @if ($seat)
+                                        <div class="seat-box">
+                                            @if ($seat->status === 'open')
+                                                <img class="standardSeatImg" src="{{ asset('images/singleseat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @else
+                                                <img class="standardSeatImg"
+                                                    src="{{ asset('images/unavailableseat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @endif
+                                            <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}
+                                            </div>
+                                        </div>
                                     @endif
                                 @endfor
                             </div>
                             <div class="standard-right-column">
-                                @for($j=10; $j<=12;$j++)
+                                @for ($j = 10; $j <= 12; $j++)
                                     @php
-                                        $seat = $seats->where('row_letter', $rowLetters[$i])->where('column_number', $j)->first();
+                                        $seat = $seats
+                                            ->where('row_letter', $rowLetters[$i])
+                                            ->where('column_number', $j)
+                                            ->first();
                                     @endphp
-                                    @if($seat)
-                                        @if($seat->status === 'open')
-                                        <img class="seatImg" src="{{ asset('images/singleseat.png')}}" alt="Seat {{ $seat->seat_id }}">
-                                        @else
-                                        <img class="seatImg" src="{{ asset('images/unavailableseat.png')}}" alt="Seat {{ $seat->seat_id }}">
-                                        @endif
+                                    @if ($seat)
+                                        <div class="seat-box">
+                                            @if ($seat->status === 'open')
+                                                <img class="standardSeatImg" src="{{ asset('images/singleseat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @else
+                                                <img class="standardSeatImg"
+                                                    src="{{ asset('images/unavailableseat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @endif
+                                            <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}
+                                            </div>
+                                        </div>
                                     @endif
                                 @endfor
                             </div>
                         </div>
                     @endfor
                 @endif
+                @if ($hall->hall_type === 'Premium')
+                    @for ($i = 0; $i < sizeof($rowLetters); $i++)
+                        <div class="premium-row">
+                            <div class="premium-left-column">
+                                @for ($j = 1; $j <= 5; $j++)
+                                    @php
+                                        $seat = $seats
+                                            ->where('row_letter', $rowLetters[$i])
+                                            ->where('column_number', $j)
+                                            ->first();
+                                    @endphp
+                                    @if ($seat)
+                                        <div class="seat-box">
+                                            @if ($seat->status === 'open')
+                                                <img class="premiumSeatImg" src="{{ asset('images/premiumSeat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @else
+                                                <img class="premiumSeatImg"
+                                                    src="{{ asset('images/unavailablePremiumSeat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @endif
+                                            <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endfor
+                            </div>
+                            <div class="premium-right-column">
+                                @for ($j = 6; $j <= 10; $j++)
+                                    @php
+                                        $seat = $seats
+                                            ->where('row_letter', $rowLetters[$i])
+                                            ->where('column_number', $j)
+                                            ->first();
+                                    @endphp
+                                    @if ($seat)
+                                        <div class="seat-box">
+                                            @if ($seat->status === 'open')
+                                                <img class="premiumSeatImg" src="{{ asset('images/premiumSeat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @else
+                                                <img class="premiumSeatImg"
+                                                    src="{{ asset('images/unavailablePremiumSeat.png') }}"
+                                                    alt="Seat {{ $seat->seat_id }}">
+                                            @endif
+                                            <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endfor
+                            </div>
+                        </div>
+                    @endfor
+                @endif
+                @if ($hall->hall_type === 'Family')
+                    @for ($i = 0; $i < sizeof($rowLetters); $i++)
+                        <div class="family-row">
+                            @php
+                                $seat = $seats->where('row_letter', $rowLetters[$i])->first();
+                            @endphp
+                            @if ($seat->status === 'open')
+                                <div class="seat-box">
+                                    <img class="familySeatImg" src="{{ asset('images/familySeat.png') }}"
+                                        alt="Seat {{ $seat->seat_id }}">
+                                    <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}</div>
+                                </div>
+                            @else
+                                <div class="seat-box">
+                                    <img class="familySeatImg" src="{{ asset('images/unavailableFamilySeat.png') }}"
+                                        alt="Seat {{ $seat->seat_id }}">
+                                    <div class="seat-info">{{ $seat->row_letter }}{{ $seat->column_number }}</div>
+                                </div>
+                            @endif
+                        </div>
+                    @endfor
+                @endif
             </div>
         </div>
-        <div class="text-end mt-3"> 
+        <div class="text-end mt-3">
             <button type="submit" class="btn btn-primary">Confirm Modify</button>
         </div>
-        <p>Number of unique row letters: {{ sizeof($rowLetters) }}</p>
-        @foreach($rowLetters as $letter)
-           <p>Current letter: {{ $letter }}</p>
-        @endforeach
-            
-        @foreach ($seats as $seat)
-        <p>Current seat: {{ $seat }}</p>
-    @endforeach
-            
     </form>
-    
-@vite(['resources/css/admin/manageHall/edit.css'], ['resources/js/admin/manageHall/edit.js'])
+
+    @vite(['resources/css/admin/manageHall/edit.css'], ['resources/js/admin/manageHall/edit.js'])
 </x-admin-Layout>
