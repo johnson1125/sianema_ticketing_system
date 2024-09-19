ba87c32c9551f89975c4d92aafd5ddc63d35378b
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-09-19 14:21:14 +0800

    minor change to homecontroller to show same movies as booking-movies page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function fetchAllMovies()
     {
-        $movies = Movie::all(); // Fetch all movies
+        $today = Carbon::today('Asia/Kuala_Lumpur');
+
+        // Fetch movies where today's date is between screen_from_date and screen_until_date
+        $movies = Movie::whereDate('screen_from_date', '<=', $today)
+            ->whereDate('screen_until_date', '>=', $today)
+            ->get();
+
+        // Return the view with the filtered movies
         return view('home', compact('movies')); // Pass movies to the view
     }
 }
