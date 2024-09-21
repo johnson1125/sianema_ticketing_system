b43dfdf0960895ec7711634e5f66a77d291fcf80
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-09-21 19:00:44 +0800

    insert user record

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,21 +13,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        // DB::table('users')->upsert([
-        //     [
-        //         'id' => 1,
-        //         'name' => 'admin',
-        //         'email' => 'abc123@gmail.com',
-        //         'email_verified_at' => NULL,
-        //         'password' => '$2y$12$t3KS8XGBxxlrT54yDwGF2.vVoDe1trK2uDKuFcbIi4xYfNrYCLNoK',
-        //         'remember_token' => NULL,
-        //         'created_at' => '2024-08-20 03:18:42',
-        //         'updated_at' => '2024-08-20 03:18:42',
-        //         'role' => 'admin',
-        //         'mobile_number' => '0123456789',
-        //         'date_of_birth' => '2001-10-27'
-        //     ],
-        // ], ['id']);
-        
+        $path = database_path('sql/users.sql');
+        $sql = file_get_contents($path);
+
+        // Execute the SQL dump
+        DB::unprepared($sql);
+
     }
 }
