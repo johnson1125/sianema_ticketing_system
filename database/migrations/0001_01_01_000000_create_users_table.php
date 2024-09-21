717ad424a8a3617d3fd722f65e0211fa8edc6b90
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-09-21 18:07:31 +0800

    update migratioons file to fix login issue

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -29,11 +29,14 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->string('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
+
+            // Define the foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
